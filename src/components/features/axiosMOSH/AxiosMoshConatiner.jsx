fc3db399e3f3fbad5385aec65c54295f054f179d
StevOrc <steven.orcun2@gmail.com>
2020-09-30 16:46:22 +0200

    mosh http add, put, delete, get and optimistic vs pesimistic

--- src/components/features/axiosMOSH/AxiosMoshConatiner.jsx
+++ src/components/features/axiosMOSH/AxiosMoshConatiner.jsx
@@ -1,28 +1,68 @@
 import React, { Component } from "react";
 import axios from 'axios';
 
+const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
+
 class AxiosMoshContainer extends Component {
   state = {
     posts: [],
+    errorDelete: null 
   };
 
   async componentDidMount(){
-    const {data: posts} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const {data: posts} = await axios.get(apiEndPoint);
     this.setState({
       posts
     })
   }
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    const obj = {title: "Test", body: "TEST TEST TEST"};
+    const {data: post} = await axios.post(apiEndPoint, obj);
+    
+    const posts = [post, ...this.state.posts];
+    this.setState({
+      posts
+    });
   };
 
-  handleUpdate = (post) => {
-    console.log("Update", post);
+  handleUpdate = async (post) => {
+    const originalPost = {...post};
+    post.title = "UPDATE";
+    let posts = [...this.state.posts];
+    const postIndex = this.state.posts.indexOf(post);
+    posts[postIndex] = {...post};
+    this.setState({
+      posts
+    });
+    try{
+      await axios.put(`${apiEndPoint}/${post.id}`, post);
+      throw new Error('');
+    }catch (ex) {
+      alert('Error when try to updtate post');
+      posts[postIndex] = {...originalPost}
+      this.setState({
+        posts
+      })
+    }
+
   };
 
-  handleDelete = (post) => {
-    console.log("Delete", post);
+  handleDelete = async (post) => {
+    const originalPosts = this.state.posts;
+    const posts = this.state.posts.filter( el => el.id !== post.id);
+    this.setState({
+      posts
+    });
+    try {
+      await axios.delete(`${apiEndPoint}/${post.id}`);
+      throw new Error(''); 
+    } catch (error) {
+      alert('Something wrong when delete post');
+      this.setState({
+        posts: originalPosts
+      })
+    }
   };
   render() {
     return (
