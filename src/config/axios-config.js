1060eeb6844915639a9ee20565d15c5bf59aa825
StevOrc <steven.orcun2@gmail.com>
2020-10-01 14:31:50 +0200

    interceptor and config axios defualts values request

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -4,3 +4,8 @@ axios.defaults.headers.common["token"] =
   "SFKHFzIHVZFHGYIRCudhcZTGrfhozuirhreuifregzroiczorfuie";
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 axios.defaults.timeout = 4000;
+
+axios.interceptors.request.use( req => {
+  console.log("REQUEST intercepted", req);
+  return req;
+})
